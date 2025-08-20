46b4d25aba8d7f332da3b1ec7dba6c70ebfe2e52
VenkataKoti <venkatakotaiahkanakam@gmail.com>
2025-08-20 16:12:19 +0530

    Shell scripting

---
+++ 02-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Jenny:: Hello Kiyansh, good morning"
+echo "Kiyansh:: Hi Jenny, Very good morning"
+echo "Jenny:: How are you Kiyansh"
+echo "Kiyansh:: I'm fine Jenny, How are you..?"
