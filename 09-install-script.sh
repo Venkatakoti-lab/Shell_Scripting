df63683d7ec25691e23f09cf88c3e58063ecaf8a
VenkataKoti <venkatakotaiahkanakam@gmail.com>
2025-10-18 18:42:54 +0530

    shell

--- 09-install-script.sh
+++ 09-install-script.sh
@@ -1,10 +1,41 @@
 #!/bin/bash
 
 USERID=$(id -u)
-if [$USERID -ne 0]
+if [ $USERID -ne 0 ]
 then
      echo "ERROR:: user must have sudo access to run this script"
      exit 1 #other than 0 we can give any number
 fi
-dnf install mysqll -y
-dnf install docker -y
+
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then #not installed mysql package
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+         echo "Installing MySql... FAILURE"
+         exit 1
+    else
+        echo "Installing MySql... SUCCESS"
+    fi
+else
+     echo "MySql is already... Installed"
+fi 
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+     dnf install git -y
+     if [ $? -ne 0 ]
+     then
+          echo "Git installed...FAILED"
+          exit 1
+     else
+          echo "Git installed...SUCCESSFULLY"
+else
+     echo "Git already...INSTALLED"
+fi 
+
+     
