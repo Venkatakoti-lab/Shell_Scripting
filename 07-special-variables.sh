27e3db781b51713b2cebe2ca6f2c38a222d8b8b4
VenkataKoti <venkatakotaiahkanakam@gmail.com>
2025-10-18 18:02:58 +0530

    shell

---
+++ 07-special-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+All variables passed : $@
+Numbers of variables: $#
+Script name: $0
+Present working directory: $PWD
+Home directory of current user: $USER 
+Process id of current script: $$
+Process id of last command in background: $!
