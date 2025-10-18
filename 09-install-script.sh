81b5dc383a9e6a05365ef65c175e9ecf1f7c270b
VenkataKoti <venkatakotaiahkanakam@gmail.com>
2025-10-18 18:26:41 +0530

    shell

--- 09-install-script.sh
+++ 09-install-script.sh
@@ -4,6 +4,7 @@ USERID=$(id -u)
 if [$USERID -ne 0]
 then
      echo "ERROR:: user must have sudo access to run this script"
+     exit 1 #other than 0 we can give any number
 fi
 dnf install mysqll -y
 dnf install maven -y
