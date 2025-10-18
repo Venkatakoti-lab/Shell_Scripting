2b1f8a2693b64e6a9d1a5d1378977ac774d3b288
VenkataKoti <venkatakotaiahkanakam@gmail.com>
2025-10-18 17:56:21 +0530

    shell

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 FRUITS=("apple" "banana" "orange" "guava")
 
-echo "first fruit name is : ${FRUITS[0]}"
+echo "First fruit name is : ${FRUITS[0]}"
+echo "Second fruit name is : ${FRUITS[1]}"
+echo "Third fruit name is : ${FRUITS[2]}"
+echo "Fourth fruit name is : ${FRUITS[3]}"
