979f39817ee4490add0a733ee8e8c8000d11a80f
VenkataKoti <venkatakotaiahkanakam@gmail.com>
2025-10-19 16:59:04 +0530

    Shell scripting

--- 10-functions.sh
+++ 10-functions.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 USERID=$(id -u)
+VALIDATE (){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2: FAILED"
+        exit 1
+    else
+        echo "$2: SUCCESS"
+    fi
+}
 if [ USERID -ne 0 ]
 then
     echo "ERROR:: please run this script as root user"
@@ -11,13 +20,7 @@ dnf list installed mysql
 if [ $? -ne 0 ]
 then #not installed
     dnf install mysql -y
-    if [ $? -ne 0 ]
-    then
-        echo "mysql installation: FAILED"
-        exit 1
-    else
-        echo "mysql installation: SUCCESS"
-    fi 
+    VALIDATE $? "mysql installation"
 else
     echo "mysql installed: ALREADY"
 fi 
@@ -26,13 +29,14 @@ dnf list installed git
 if [ $? -ne 0 ]
 then
     dnf install git -y
-    if [ $? -ne 0 ]
-    then
-        echo "git installation: FAILED"
-        exit 1
-    else
-        echo "git installation: SUCCESS"
-    fi 
+    VALIDATE $? "installtion git"
+    # if [ $? -ne 0 ]
+    # then
+    #     echo "git installation: FAILED"
+    #     exit 1
+    # else
+    #     echo "git installation: SUCCESS"
+    # fi 
 else
     echo "git installed: ALREADY"
 fi 
